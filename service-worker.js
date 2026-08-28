48b9d15331d621c8e7cf9d552dc0b0276b77745a
nickwoloszyn00-eng <nickwoloszyn00@gmail.com>
2026-08-28 17:33:18 -0500

    Refresh premium town assets offline

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'puzzle-empire-v2-android-3';
+const CACHE_NAME = 'puzzle-empire-v2-android-4';
 const APP_FILES = ['./','./index.html','./styles.css','./levels.js','./game.js','./auth-config.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(Promise.all([
