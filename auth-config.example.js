7bd9a7b7584f62280020b3943530d988f1b18d52
nickwoloszyn00-eng <nickwoloszyn00@gmail.com>
2026-08-28 16:21:07 -0500

    Publish Puzzle Empire prototype

---
+++ auth-config.example.js
@@ -0,0 +1,6 @@
+/* Copy these values to auth-config.js when your Google OAuth app is created. */
+window.PUZZLE_EMPIRE_AUTH = {
+  googleClientId: 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+  profileApiUrl: 'https://api.yourdomain.com/auth/google'
+};
+
