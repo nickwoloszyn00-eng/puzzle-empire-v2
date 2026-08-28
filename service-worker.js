c4a4a19c0d31b73b7090fb03e81d446708f2c051
nickwoloszyn00-eng <nickwoloszyn00@gmail.com>
2026-08-28 16:20:52 -0500

    Publish Puzzle Empire prototype

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'puzzle-empire-v2-android-1';
+const APP_FILES = ['./','./index.html','./styles.css','./levels.js','./game.js','./auth-config.js','./manifest.webmanifest','./assets/icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (new URL(event.request.url).origin === location.origin) {
+      const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
+
